6a46fde7320310b0b175ff3c16d1b32f6b28e491
sasakiryo <potatoqueen06@gmail.com>
2020-01-16 20:05:19 +0900

    Updates

---
+++ precache-manifest.f5c63a10e15d2f145ee38c9ede4013cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f646daff67b88f6aff710c5c3ccff62",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "74f57dbd05b1845058fe",
+    "url": "/react-practice/static/js/2.ada6d71a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-practice/static/js/2.ada6d71a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbbb31af5d2175bec7ab",
+    "url": "/react-practice/static/js/main.fb1cdcbc.chunk.js"
+  },
+  {
+    "revision": "53b924415c33807c2874",
+    "url": "/react-practice/static/js/runtime-main.e74fe1bf.js"
+  }
+]);
